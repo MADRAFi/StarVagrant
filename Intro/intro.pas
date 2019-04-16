e55e5317b0e206a283ba87a702f81d6cc7b0aacb
MADRAFi <madrafi@gmail.com>
2019-04-16 21:52:22 +0200

    finished screens with all texts

--- Intro/intro.pas
+++ Intro/intro.pas
@@ -1,7 +1,7 @@
 {$librarypath '../../MADS/lib/'}
 {$librarypath '../../MADS/base/'}
 {$librarypath '../../MADS/blibs/'}
-uses atari, b_utils, b_system, b_crt, sysutils, cmc;
+uses atari, b_utils, b_system, b_crt, sysutils, rmt; //cmc;
 
 const
 {$i const.inc}
@@ -16,7 +16,8 @@ var
     $10,$14,$1a,$00
   );
   strings: array [0..0] of Word absolute STRINGS_ADDRESS;
-  msx: TCMC;
+  // msx: TCMC;
+  msx: TRMT;
   txt: String;
   line: Word;
   skip: Boolean;
@@ -40,21 +41,12 @@ end;
 procedure gfx_fadein;
 
 begin
-  gfxcolors[0]:=0;
-  gfxcolors[1]:=0;
-  gfxcolors[2]:=0;
-  gfxcolors[3]:=0;
-
   repeat
     Waitframes(2);
-    If (gfxcolors[0] and %00001111 < piccolors[0] and %00001111 ) then Inc(gfxcolors[0]) else gfxcolors[0]:=piccolors[0];
-    If (gfxcolors[1] and %00001111 < piccolors[1] and %00001111) then Inc(gfxcolors[1]) else gfxcolors[1]:=piccolors[1];
-    If (gfxcolors[2] and %00001111 < piccolors[2] and %00001111) then Inc(gfxcolors[2]) else gfxcolors[2]:=piccolors[2];
-    If (gfxcolors[3] and %00001111 < piccolors[3] and %00001111) then Inc(gfxcolors[3]) else gfxcolors[3]:=piccolors[3];
-
-      // If (txtcolors[0] and %00001111 < 0 and %00001111) then inc(txtcolors[0]) else txtcolors[0]:=0;
-      // If (txtcolors[1] and %00001111 < $1c and %00001111) then inc(txtcolors[1]) else txtcolors[1]:=$1c;
-
+    If (gfxcolors[0] and %00001111 <> piccolors[0] and %00001111) then Inc(gfxcolors[0]) else gfxcolors[0]:=piccolors[0];
+    If (gfxcolors[1] and %00001111 <> piccolors[1] and %00001111) then Inc(gfxcolors[1]) else gfxcolors[1]:=piccolors[1];
+    If (gfxcolors[2] and %00001111 <> piccolors[2] and %00001111) then Inc(gfxcolors[2]) else gfxcolors[2]:=piccolors[2];
+    If (gfxcolors[3] and %00001111 <> piccolors[3] and %00001111) then Inc(gfxcolors[3]) else gfxcolors[3]:=piccolors[3];
   until (gfxcolors[0]=piccolors[0]) and (gfxcolors[1]=piccolors[1]) and (gfxcolors[2]=piccolors[2]) and (gfxcolors[3]=piccolors[3]);
 end;
 
@@ -142,144 +134,165 @@ end;
 begin
   fadeoff;
   SystemOff;
+
+  // poke($0c, $f2);
+
+  // Initialize player
+  msx.player:=pointer(PLAYER_ADDRESS);
+  msx.modul:=pointer(MODULE_ADDRESS);
+  msx.init(0);
+  // msx.player:=pointer(CMC_PLAYER_ADDRESS);
+  // msx.modul:=pointer(CMC_MODULE_ADDRESS);
+  // msx.init;
+
   skip:=false;
+
+  // clear gfx memory
   fillbyte(pointer(GFX_ADDRESS), 7684, 0);
   SetCharset (Hi(CHARSET_ADDRESS)); // when system is off
   //CRT_Init(GFX_ADDRESS);
 
-  // VDSLST:=Word(@DLI_TITLE1);
+  VDSLST:=Word(@DLI_TITLE1);
   EnableVBLI(@vbl_title);
   EnableDLI(@dli_title1);
   Waitframe;
   DLISTL:= DISPLAY_LIST_ADDRESS_TITLE;
 
+  waitframes(150);
 
-  // Initialize player
-  // msx.player:=pointer(RMT_PLAYER_ADDRESS);
-  // msx.modul:=pointer(RMT_MODULE_ADDRESS);
-  // msx.init(0);
-  msx.player:=pointer(CMC_PLAYER_ADDRESS);
-  msx.modul:=pointer(CMC_MODULE_ADDRESS);
-  msx.init;
 
-  waitframes(100);
 
   if skip = false then begin
+    gfx_fadeout;
     txt:=FFTermToString(strings[0]);
-    putString(10,20,txt);
+    putString(0,20,txt);
     gfx_fadein;
-    waitframes(100);
+    waitframes(150);
+  end;
+
+  if skip = false then begin
+    gfx_fadeout;
+    putString(0,20,'                    '~);
+    // putString(0,21,'                    '~);
+    txt:=FFTermToString(strings[1]);
+    putString(0,20,txt);
+    txt:=FFTermToString(strings[2]);
+    putString(0,21,txt);
+    gfx_fadein;
+    waitframes(150);
   end;
 
   if skip = false then begin
     gfx_fadeout;
     move(pointer(PIC1_ADDRESS), pointer(GFX_ADDRESS), 7684);
-    putString(0,20,'                    '~);
-    putString(0,21,'                    '~);
-    // line:=GFX_ADDRESS + word(20 * 40 * 8)-(40*2) + (0 * 2);
-    // 20 - y, 40 - screen size,  8 char size, 2 - 2 line before, 0 - x
-    txt:=FFTermToString(strings[1]);
+    // putString(0,20,'                    '~);
+    // putString(0,21,'                    '~);
+    // // line:=GFX_ADDRESS + word(20 * 40 * 8)-(40*2) + (0 * 2);
+    // // 20 - y, 40 - screen size,  8 char size, 2 - 2 line before, 0 - x
     line:=GFX_ADDRESS + 6320;
     fillbyte(pointer(line), 80, 0); // 2 lines before
+    txt:=FFTermToString(strings[3]);
     putString(0,20,txt);
-    txt:=FFTermToString(strings[2]);
-    line:=GFX_ADDRESS + 6800;
-    fillbyte(pointer(line), 80, 0); // 2 lines after
+    txt:=FFTermToString(strings[4]);
     putString(0,21,txt);
+    line:=GFX_ADDRESS + 7040;  // 22 * 40 * 8 - next line after text
+    fillbyte(pointer(line), 80, 0); // 2 lines after
     gfx_fadein;
-    waitframes(250);waitframes(250);
+    waitframes(150);
+  end;
+
+  if skip = false then begin
+    putString(0,20,'                    '~);
+    putString(0,21,'                    '~);
+    line:=GFX_ADDRESS + 6320;
+    fillbyte(pointer(line), 80, 0);
+    txt:=FFTermToString(strings[5]);
+    putString(0,20,txt);
+    txt:=FFTermToString(strings[6]);
+    putString(0,21,txt);
+    line:=GFX_ADDRESS + 7040;
+    fillbyte(pointer(line), 80, 0);
+    waitframes(150);
   end;
 
   if skip = false then begin
     gfx_fadeout;
     move(pointer(PIC2_ADDRESS), pointer(GFX_ADDRESS), 7684);
-    putString(0,20,'                    '~);
-    putString(0,21,'                    '~);
-    txt:=FFTermToString(strings[3]);
+    // putString(0,20,'                    '~);
+    // putString(0,21,'                    '~);
     line:=GFX_ADDRESS + 6320;
     fillbyte(pointer(line), 80, 0);
+    txt:=FFTermToString(strings[7]);
     putString(0,20,txt);
-    txt:=FFTermToString(strings[4]);
-    line:=GFX_ADDRESS + 6800;
-    fillbyte(pointer(line), 80, 0);
+    txt:=FFTermToString(strings[8]);
     putString(0,21,txt);
+    line:=GFX_ADDRESS + 7040;
+    fillbyte(pointer(line), 80, 0);
     gfx_fadein;
-    waitframes(250);
+    waitframes(150);
   end;
   if skip = false then begin
     putString(0,20,'                    '~);
     putString(0,21,'                    '~);
-    txt:=FFTermToString(strings[5]);
     line:=GFX_ADDRESS + 6320;
     fillbyte(pointer(line), 80, 0);
+    txt:=FFTermToString(strings[9]);
     putString(0,20,txt);
-    txt:=FFTermToString(strings[6]);
-    line:=GFX_ADDRESS + 6800;
-    fillbyte(pointer(line), 80, 0);
+    txt:=FFTermToString(strings[10]);
     putString(0,21,txt);
-
-    waitframes(250);
+    txt:=FFTermToString(strings[11]);
+    putString(0,22,txt);
+    line:=GFX_ADDRESS + 7360;
+    fillbyte(pointer(line), 80, 0);
+    waitframes(150);waitframes(150);
   end;
 
   if skip = false then begin
     gfx_fadeout;
+    move(pointer(PIC3_ADDRESS), pointer(GFX_ADDRESS), 7684);
+    // putString(0,20,'                    '~);
+    // putString(0,21,'                    '~);
+    line:=GFX_ADDRESS + 6320;
+    fillbyte(pointer(line), 80, 0);
+    txt:=FFTermToString(strings[12]);
+    putString(0,20,txt);
+    txt:=FFTermToString(strings[13]);
+    putString(0,21,txt);
+    line:=GFX_ADDRESS + 7040;
+    fillbyte(pointer(line), 80, 0);
+    gfx_fadein;
+    waitframes(150);
+  end;
+  if skip = false then begin
+    putString(0,20,'                    '~);
+    putString(0,21,'                    '~);
+    line:=GFX_ADDRESS + 6320;
+    fillbyte(pointer(line), 80, 0);
+    txt:=FFTermToString(strings[14]);
+    putString(0,20,txt);
+    txt:=FFTermToString(strings[15]);
+    putString(0,21,txt);
+    txt:=FFTermToString(strings[16]);
+    putString(0,22,txt);
+    line:=GFX_ADDRESS + 7360;
+    fillbyte(pointer(line), 80, 0);
+    waitframes(150);waitframes(150);
+  end;
+  if skip = false then begin
+    gfx_fadeout;
     // putString(0,22,'                    '~);
     fillbyte(pointer(GFX_ADDRESS), 7684, 0);
-
-
     // putString(0,20,'                    '~);
     // putString(0,21,'                    '~);
     txt:=FFTermToString(strings[17]);
-    // line1:=GFX_ADDRESS + word(20 * 320)-(40*2) + (0 * 2);
-    // fillbyte(pointer(line1), 40*2, 0);
     putString(0,20,txt);
     txt:=FFTermToString(strings[18]);
-    // line2:=GFX_ADDRESS + word(21 * 320)+(40*2) + (0 * 2);
-    // fillbyte(pointer(line2), 40*2, 0);
     putString(0,21,txt);
     gfx_fadein;
-
-    waitframes(250);waitframes(250);
+    waitframes(150);//waitframes(250);
   end;
 
-  // txt:=FFTermToString(strings[1]);
-  // putString(0,140,txt);
-  // gfx_fadein;
-  // waitframes(100);
-  //
-  // gfx_fadeout;
-  // fillbyte(dest,word(txt[0]),$0);
-  // txt:=FFTermToString(strings[2]);
-  // putString(0,140,txt);
-  // gfx_fadein;
-  // waitframes(100);
-  //
-  // gfx_fadeout;
-  // fillbyte(dest,word(txt[0]),0);
-  // txt:=FFTermToString(strings[3]);
-  // putString(0,140,txt);
-  // gfx_fadein;
-  // waitframes(100);
-
-  // gfx_fadeout;
-  // putString(0,0,'                    '~);
-  // // txt:=FFTermToString(strings[4]);
-  // // putString(0,140,txt);
-  // putString(0,5,'Ala ma kota'~);
-  // gfx_fadein;
-  // waitframes(255);
-  //
-  // gfx_fadeout;
-  // putString(0,5,'                    '~);
-  // txt:=FFTermToString(strings[6]);
-  // putString(0,6,txt);
-  // gfx_fadein;
-  // waitframes(300);
-
-  repeat
-    waitframe;
-    // msx.play;
-  until CRT_keypressed;
+  gfx_fadeout;
   msx.stop;
   DisableDLI;
   DisableVBLI;
--- Intro/strings.asm
+++ Intro/strings.asm
@@ -24,41 +24,41 @@ strings
 txt_0
     dta d'Year 2419,',$ff
 txt_1
-    dta d'United Earth Empire',$ff
+    dta d'United Earth Empire ',$ff
 txt_2
-    dta d'is still fighting.',$ff
+    dta d'is still fighting.  ',$ff
 txt_3
-    dta d'After 8 years in',$ff
+    dta d'After 8 years in    ',$ff
 txt_4
-	  dta d'military, you have',$ff
+	  dta d'military, you have  ',$ff
 txt_5
-	  dta d'finally fulfilled',$ff
+	  dta d'finally fulfilled   ',$ff
 txt_6
-	  dta d'your contract.',$ff
+	  dta d'your contract.      ',$ff
 txt_7
-    dta d'For a long time you',$ff
+    dta d'For a long time you ',$ff
 txt_8
-    dta d'dreamed about your',$ff
+    dta d'dreamed about your  ',$ff
 txt_9
-    dta d'own venture and to',$ff
+    dta d'own venture and to  ',$ff
 txt_10
-    dta d'be able to do',$ff
+    dta d'be able to do       ',$ff
 txt_11
-    dta d'whatever you want.',$ff
+    dta d'whatever you want.  ',$ff
 txt_12
-    dta d'With received pay',$ff
+    dta d'With received pay   ',$ff
 txt_13
-    dta d'check you were able',$ff
+    dta d'check you were able ',$ff
 txt_14
     dta d'to buy cheapest ship',$ff
 txt_15
-    dta d'and still have some',$ff
+    dta d'and still have some ',$ff
 txt_16
-    dta d'change left.',$ff
+    dta d'change left.        ',$ff
 txt_17
-    dta d'It is up to you',$ff
+    dta d'It is up to you     ',$ff
 txt_18
-    dta d'where to go now...',$ff
+    dta d'where to go now...  ',$ff
 
 
  .print "STRINGS SIZE: ", *-strings_list
