56f819fd752b88411838d6e37adb91269e2bd0d1
MADRAFi <madrafi@gmail.com>
2019-10-29 00:48:32 +0100

    DE translation fixes

--- StarV/makesv.bat
+++ StarV/makesv.bat
@@ -2,5 +2,6 @@
 c:\Users\MADRAFi\Dropbox\Atari\DEV\MADS\mp.exe starv.pas -code:0c00 -define:EN
 c:\Users\MADRAFi\Dropbox\Atari\DEV\MADS\mads.exe starv.a65 -x -i:c:\Users\MADRAFi\Dropbox\Atari\DEV\MADS\base -o:starv.xex
 copy /Y starv.xex ..\..\Release\SourceDisk\starv.xex
+copy /Y ..\Intro\EN\intro.xex ..\..\Release\SourceDisk\intro.xex
 D:\Atari\Soft\tools\dir2atr -md -B D:\Atari\Soft\tools\xboot.obx c:\Users\MADRAFi\Dropbox\Atari\DEV\StarVagrant\Release\starvagrant.atr c:\Users\MADRAFi\Dropbox\Atari\DEV\StarVagrant\Release\SourceDisk\
 pause
