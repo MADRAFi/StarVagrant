a2be51fe2e2282c540929efc47591824aa6e7d3b
MADRAFi <madrafi@gmail.com>
2019-12-27 20:39:15 +0100

    PL translation fix and cleanup

--- loader/make.bat
+++ loader/make.bat
@@ -1,3 +1,5 @@
+rem c:\Users\MADRAFi\Dropbox\Atari\DEV\MADS\mads.exe loader.asm -x -i:c:\Users\MADRAFi\Dropbox\Atari\DEV\MADS\base -o:Xautorun.xex
 rem c:\Users\MADRAFi\Dropbox\Atari\DEV\MADS\mads.exe loader.asm -x -i:c:\Users\MADRAFi\Dropbox\Atari\DEV\MADS\base -o:loader_nointro.xex
-c:\Users\MADRAFi\Dropbox\Atari\DEV\MADS\mads.exe loader.asm -x -i:c:\Users\MADRAFi\Dropbox\Atari\DEV\MADS\base -o:Xautorun.xex
+c:\Users\MADRAFi\Dropbox\Atari\DEV\MADS\mads.exe loader.asm -x -i:c:\Users\MADRAFi\Dropbox\Atari\DEV\MADS\base -o:loader_nogame.xex
+
 
