c589c885bf2a53116edbf9591b36f38ceb22e977
MADRAFi <madrafi@gmail.com>
2020-01-14 14:44:39 +0100

    fade fixes

--- Intro/intro.pas
+++ Intro/intro.pas
@@ -65,6 +65,25 @@ piccolors: array [0..(4*NUMBEROFPICS)-1] of Byte = (
 
 {$i interrupts.inc}
 
+procedure gfx_fadeout; //(hidetext: Boolean);
+var
+hidetext: boolean = true;
+begin
+
+  repeat
+    Waitframes(2);
+    for b:=0 to 3 do 
+              If (gfxcolors[b] and %00001111 <> 0) then Dec(gfxcolors[b]) else gfxcolors[b]:=0;
+
+      If (txtcolors[0] and %00001111 <> 0) then Dec(txtcolors[0]) else txtcolors[0]:=0;
+      If (txtcolors[1] and %00001111 <> 0) then Dec(txtcolors[1]) else txtcolors[1]:=0;
+
+until ((gfxcolors[0] or gfxcolors[1] or gfxcolors[2] or gfxcolors[3])=0);
+  //  until ((gfxcolors[0] or gfxcolors[1] or gfxcolors[2] or gfxcolors[3])=0) and ((txtcolors[0] or txtcolors[1])=0);
+  //  waitframes(10);
+end;
+
+
 procedure gfx_fadein;
 const
   TXTCOLOR = $1c;
@@ -74,58 +93,17 @@ begin
   y:= picnumber shl 2;
   repeat
     Waitframes(2);
-    // If (gfxcolors[0] and %00001111 <> piccolors[y] and %00001111) then Inc(gfxcolors[0]) else gfxcolors[0]:=piccolors[y];
-    // If (gfxcolors[1] and %00001111 <> piccolors[y+1] and %00001111) then Inc(gfxcolors[1]) else gfxcolors[1]:=piccolors[y+1];
-    // If (gfxcolors[2] and %00001111 <> piccolors[y+2] and %00001111) then Inc(gfxcolors[2]) else gfxcolors[2]:=piccolors[y+2];
-    // If (gfxcolors[3] and %00001111 <> piccolors[y+3] and %00001111) then Inc(gfxcolors[3]) else gfxcolors[3]:=piccolors[y+3];
     for b:=0 to 3 do 
       If ((gfxcolors[b] and %00001111) <= (piccolors[y+b] and %00001111)) then Inc(gfxcolors[b]) else gfxcolors[b]:=piccolors[y+b];
 
-    If (txtcolors[0] and %00001111 <> TXTBACK and %00001111) then Inc(txtcolors[0]) else txtcolors[0]:=TXTBACK;
-    If (txtcolors[1] and %00001111 <> TXTCOLOR and %00001111) then Inc(txtcolors[1]) else txtcolors[1]:=TXTCOLOR;
+    If ((txtcolors[0] and %00001111) <= (TXTBACK and %00001111)) then Inc(txtcolors[0]) else txtcolors[0]:=TXTBACK;
+    If ((txtcolors[1] and %00001111) <= (TXTCOLOR and %00001111)) then Inc(txtcolors[1]) else txtcolors[1]:=TXTCOLOR;
 
-  until (gfxcolors[0]=piccolors[y]) and
+  until ((gfxcolors[0]=piccolors[y]) and
         (gfxcolors[1]=piccolors[y+1]) and
         (gfxcolors[2]=piccolors[y+2]) and
-        (gfxcolors[3]=piccolors[y+3]);
+        (gfxcolors[3]=piccolors[y+3])) or ((txtcolors[0]=TXTBACK) and (txtcolors[1]=TXTCOLOR));
 end;
-
-procedure gfx_fadeout; //(hidetext: Boolean);
-begin
-
-  repeat
-    Waitframes(2);
-    If (gfxcolors[0] and %00001111 <> 0) then Dec(gfxcolors[0]) else gfxcolors[0]:=0;
-    If (gfxcolors[1] and %00001111 <> 0) then Dec(gfxcolors[1]) else gfxcolors[1]:=0;
-    If (gfxcolors[2] and %00001111 <> 0) then Dec(gfxcolors[2]) else gfxcolors[2]:=0;
-    If (gfxcolors[3] and %00001111 <> 0) then Dec(gfxcolors[3]) else gfxcolors[3]:=0;
-    // If hidetext then
-    // begin
-      If (txtcolors[0] and %00001111 <> 0) then Dec(txtcolors[0]) else txtcolors[0]:=0;
-      If (txtcolors[1] and %00001111 <> 0) then Dec(txtcolors[1]) else txtcolors[1]:=0;
-    // end;
-  until (gfxcolors[0] or gfxcolors[1] or gfxcolors[2] or gfxcolors[3] or txtcolors[0] or txtcolors[1]) = 0;
-  // until (gfxcolors[0] or gfxcolors[1] or gfxcolors[2] or gfxcolors[3]) = 0;
-
-end;
-
-// procedure back_to_loader;assembler;
-//   asm {
-//         clc
-//         rts
-//       };
-// end;
-
-// procedure SystemOn;assembler;
-//   asm {
-//         lda:rne vcount
-//         mva #$ff portb
-//         dec nmien
-//         cli
-
-//         rts
-//     };
-// end;
 
 procedure wait(time:Word);
 begin
@@ -167,107 +145,107 @@ begin
   // clear screen memory
   fillbyte(pointer(TXT_ADDRESS), 1320, 0);
 
-  // wait(75);
+  wait(75);
 
-  // if skip = false then begin
-  //   gfx_fadeout;
-  //   CRT_GotoXY(0,24);
-  //   CRT_Write(strings[0]);
-  //   gfx_fadein;
-  // end;
+  if skip = false then begin
+    gfx_fadeout;
+    CRT_GotoXY(0,24);
+    CRT_Write(strings[0]);
+    gfx_fadein;
+  end;
 
-  // wait(450);
+  wait(450);
 
-  // if skip = false then begin
-  //   gfx_fadeout;
-  //   CRT_ClearRow(24);
-  //   CRT_WriteXY(0,24, strings[1]);
-  //   gfx_fadein;
-  // end;
+  if skip = false then begin
+    gfx_fadeout;
+    CRT_ClearRow(24);
+    CRT_WriteXY(0,24, strings[1]);
+    gfx_fadein;
+  end;
 
-  // wait(450);
+  wait(450);
 
-  // if skip = false then begin
-  //   gfx_fadeout;
-  //   picnumber:=0;
+  if skip = false then begin
+    gfx_fadeout;
+    picnumber:=0;
 
 
-  //   Waitframe;
-  //   firstDLI:=@dli_pic1_f1;
-  //   DLISTL:= DISPLAY_LIST_ADDRESS_PIC1;
+    Waitframe;
+    firstDLI:=@dli_pic1_f1;
+    DLISTL:= DISPLAY_LIST_ADDRESS_PIC1;
 
-  //   // fillbyte(pointer(TXT_ADDRESS), 1380, 0);
-  //   move(pointer(SCREEN_ADDRESS_PIC1), pointer(TXT_ADDRESS), 1200);
+    // fillbyte(pointer(TXT_ADDRESS), 1380, 0);
+    move(pointer(SCREEN_ADDRESS_PIC1), pointer(TXT_ADDRESS), 1200);
 
-  //   CRT_ClearRow(24);
-  //   CRT_ClearRow(25);
-  //   CRT_ClearRow(26);
-  //   CRT_WriteXY(0,24, strings[2]);
-  //   CRT_WriteXY(0,25, strings[3]);
-  //   gfx_fadein;
-  // end;
+    CRT_ClearRow(24);
+    CRT_ClearRow(25);
+    CRT_ClearRow(26);
+    CRT_WriteXY(0,24, strings[2]);
+    CRT_WriteXY(0,25, strings[3]);
+    gfx_fadein;
+  end;
 
-  // wait(450);
+  wait(450);
 
-  // if skip = false then begin
-  //   gfx_fadeout;
-  //   picnumber:=1;
+  if skip = false then begin
+    gfx_fadeout;
+    picnumber:=1;
 
-  //   Waitframe;
-  //   firstDLI:=@dli_pic2_f1;
-  //   DLISTL:= DISPLAY_LIST_ADDRESS_PIC2;
+    Waitframe;
+    firstDLI:=@dli_pic2_f1;
+    DLISTL:= DISPLAY_LIST_ADDRESS_PIC2;
 
-  //   move(pointer(SCREEN_ADDRESS_PIC2), pointer(TXT_ADDRESS), 1200);
+    move(pointer(SCREEN_ADDRESS_PIC2), pointer(TXT_ADDRESS), 1200);
 
-  //   CRT_ClearRow(24);
-  //   CRT_ClearRow(25);
-  //   CRT_ClearRow(26);
-  //   CRT_WriteXY(0,24, strings[4]);
-  //   CRT_WriteXY(0,25, strings[5]);
-  //   CRT_WriteXY(0,26, strings[6]);
-  //   gfx_fadein;
-  // end;
+    CRT_ClearRow(24);
+    CRT_ClearRow(25);
+    CRT_ClearRow(26);
+    CRT_WriteXY(0,24, strings[4]);
+    CRT_WriteXY(0,25, strings[5]);
+    CRT_WriteXY(0,26, strings[6]);
+    gfx_fadein;
+  end;
 
-  // wait(450);
+  wait(450);
 
-  // if skip = false then begin
-  //   gfx_fadeout;
-  //   picnumber:=2;
+  if skip = false then begin
+    gfx_fadeout;
+    picnumber:=2;
     
-  //   Waitframe;
-  //   firstDLI:=@dli_pic3_f1;
-  //   DLISTL:= DISPLAY_LIST_ADDRESS_PIC3;
+    Waitframe;
+    firstDLI:=@dli_pic3_f1;
+    DLISTL:= DISPLAY_LIST_ADDRESS_PIC3;
 
-  //   move(pointer(SCREEN_ADDRESS_PIC3), pointer(TXT_ADDRESS), 1200);
+    move(pointer(SCREEN_ADDRESS_PIC3), pointer(TXT_ADDRESS), 1200);
 
-  //   CRT_ClearRow(24);
-  //   CRT_ClearRow(25);
-  //   CRT_ClearRow(26);
-  //   CRT_WriteXY(0,24, strings[7]);
-  //   CRT_WriteXY(0,25, strings[8]);
-  //   CRT_WriteXY(0,26, strings[9]);
-  //   gfx_fadein;
-  // end;
+    CRT_ClearRow(24);
+    CRT_ClearRow(25);
+    CRT_ClearRow(26);
+    CRT_WriteXY(0,24, strings[7]);
+    CRT_WriteXY(0,25, strings[8]);
+    CRT_WriteXY(0,26, strings[9]);
+    gfx_fadein;
+  end;
 
-  // wait(450);
+  wait(450);
 
-  // if skip = false then begin
-  //   gfx_fadeout;
+  if skip = false then begin
+    gfx_fadeout;
 
-  //   Waitframe;
-  //   firstDLI:=@dli_title;
-  //   DLISTL:= DISPLAY_LIST_ADDRESS_TITLE;
+    Waitframe;
+    firstDLI:=@dli_title;
+    DLISTL:= DISPLAY_LIST_ADDRESS_TITLE;
 
-  //   // clear screen memory
-  //   fillbyte(pointer(TXT_ADDRESS), 1320, 0);
-  //   SetCharset (Hi(CHARSET_ADDRESS)); // when system is off
+    // clear screen memory
+    fillbyte(pointer(TXT_ADDRESS), 1320, 0);
+    SetCharset (Hi(CHARSET_ADDRESS)); // when system is off
 
-  //   CRT_GotoXY(0,24);
-  //   CRT_Write(strings[10]);
-  //   gfx_fadein;
-  // end;
+    CRT_GotoXY(0,24);
+    CRT_Write(strings[10]);
+    gfx_fadein;
+  end;
 
-  // wait(450);
+  wait(450);
 
   if skip = false then begin
     gfx_fadeout;
@@ -288,27 +266,27 @@ begin
         CRT_WriteCentered(y + 15, creditstxt[y]);
       end;
     // CRT_ClearRow(25);  
-    // CRT_WriteCentered(24,strings[11]);
+    CRT_WriteCentered(24,strings[11]);
     gfx_fadein;
   end;
 
   count:= 0;
   repeat 
-    // inc(count);
+    inc(count);
     waitframe;
-//     if count = 50 then
-//     begin
-// {$IFDEF PL}
-//     CRT_Invert(3,25,34);
-// {$ENDIF}
-// {$IFDEF DE}
-//     CRT_Invert(1,25,38);
-// {$ENDIF}
-// {$IFDEF EN}
-//     CRT_Invert(5,25,29);
-// {$ENDIF}
-//     count:= 0;
-//     end;
+    if count = 50 then
+    begin
+{$IFDEF PL}
+    CRT_Invert(2,24,34);
+{$ENDIF}
+{$IFDEF DE}
+    CRT_Invert(0,24,38);
+{$ENDIF}
+{$IFDEF EN}
+    CRT_Invert(5,24,29);
+{$ENDIF}
+    count:= 0;
+    end;
     if CRT_Keypressed then skip:= true;
   until skip; //or (count > 450);
 
