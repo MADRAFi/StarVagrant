d2d2444b2715bc4eb07e3949329a1afdceaee451
MADRAFi <madrafi@gmail.com>
2020-01-14 13:09:07 +0100

    intro testing

--- Intro/intro.pas
+++ Intro/intro.pas
@@ -137,6 +137,12 @@ begin
   until skip or (count > time);
 end;
 
+procedure CRT_ClearRows(start: Byte;num: Byte);
+begin
+  for y:=start to num do
+    CRT_ClearRow(y);
+end;
+
 begin
   //fadeoff;
   SystemOff;
@@ -161,107 +167,107 @@ begin
   // clear screen memory
   fillbyte(pointer(TXT_ADDRESS), 1320, 0);
 
-  wait(75);
+  // wait(75);
 
-  if skip = false then begin
-    gfx_fadeout;
-    CRT_GotoXY(0,24);
-    CRT_Write(strings[0]);
-    gfx_fadein;
-  end;
+  // if skip = false then begin
+  //   gfx_fadeout;
+  //   CRT_GotoXY(0,24);
+  //   CRT_Write(strings[0]);
+  //   gfx_fadein;
+  // end;
 
-  wait(450);
+  // wait(450);
 
-  if skip = false then begin
-    gfx_fadeout;
-    CRT_ClearRow(24);
-    CRT_WriteXY(0,24, strings[1]);
-    gfx_fadein;
-  end;
+  // if skip = false then begin
+  //   gfx_fadeout;
+  //   CRT_ClearRow(24);
+  //   CRT_WriteXY(0,24, strings[1]);
+  //   gfx_fadein;
+  // end;
 
-  wait(450);
+  // wait(450);
 
-  if skip = false then begin
-    gfx_fadeout;
-    picnumber:=0;
+  // if skip = false then begin
+  //   gfx_fadeout;
+  //   picnumber:=0;
 
 
-    Waitframe;
-    firstDLI:=@dli_pic1_f1;
-    DLISTL:= DISPLAY_LIST_ADDRESS_PIC1;
+  //   Waitframe;
+  //   firstDLI:=@dli_pic1_f1;
+  //   DLISTL:= DISPLAY_LIST_ADDRESS_PIC1;
 
-    // fillbyte(pointer(TXT_ADDRESS), 1380, 0);
-    move(pointer(SCREEN_ADDRESS_PIC1), pointer(TXT_ADDRESS), 1200);
+  //   // fillbyte(pointer(TXT_ADDRESS), 1380, 0);
+  //   move(pointer(SCREEN_ADDRESS_PIC1), pointer(TXT_ADDRESS), 1200);
 
-    CRT_ClearRow(24);
-    CRT_ClearRow(25);
-    CRT_ClearRow(26);
-    CRT_WriteXY(0,24, strings[2]);
-    CRT_WriteXY(0,25, strings[3]);
-    gfx_fadein;
-  end;
+  //   CRT_ClearRow(24);
+  //   CRT_ClearRow(25);
+  //   CRT_ClearRow(26);
+  //   CRT_WriteXY(0,24, strings[2]);
+  //   CRT_WriteXY(0,25, strings[3]);
+  //   gfx_fadein;
+  // end;
 
-  wait(450);
+  // wait(450);
 
-  if skip = false then begin
-    gfx_fadeout;
-    picnumber:=1;
+  // if skip = false then begin
+  //   gfx_fadeout;
+  //   picnumber:=1;
 
-    Waitframe;
-    firstDLI:=@dli_pic2_f1;
-    DLISTL:= DISPLAY_LIST_ADDRESS_PIC2;
+  //   Waitframe;
+  //   firstDLI:=@dli_pic2_f1;
+  //   DLISTL:= DISPLAY_LIST_ADDRESS_PIC2;
 
-    move(pointer(SCREEN_ADDRESS_PIC2), pointer(TXT_ADDRESS), 1200);
+  //   move(pointer(SCREEN_ADDRESS_PIC2), pointer(TXT_ADDRESS), 1200);
 
-    CRT_ClearRow(24);
-    CRT_ClearRow(25);
-    CRT_ClearRow(26);
-    CRT_WriteXY(0,24, strings[4]);
-    CRT_WriteXY(0,25, strings[5]);
-    CRT_WriteXY(0,26, strings[6]);
-    gfx_fadein;
-  end;
+  //   CRT_ClearRow(24);
+  //   CRT_ClearRow(25);
+  //   CRT_ClearRow(26);
+  //   CRT_WriteXY(0,24, strings[4]);
+  //   CRT_WriteXY(0,25, strings[5]);
+  //   CRT_WriteXY(0,26, strings[6]);
+  //   gfx_fadein;
+  // end;
 
-  wait(450);
+  // wait(450);
 
-  if skip = false then begin
-    gfx_fadeout;
-    picnumber:=2;
+  // if skip = false then begin
+  //   gfx_fadeout;
+  //   picnumber:=2;
     
-    Waitframe;
-    firstDLI:=@dli_pic3_f1;
-    DLISTL:= DISPLAY_LIST_ADDRESS_PIC3;
+  //   Waitframe;
+  //   firstDLI:=@dli_pic3_f1;
+  //   DLISTL:= DISPLAY_LIST_ADDRESS_PIC3;
 
-    move(pointer(SCREEN_ADDRESS_PIC3), pointer(TXT_ADDRESS), 1200);
+  //   move(pointer(SCREEN_ADDRESS_PIC3), pointer(TXT_ADDRESS), 1200);
 
-    CRT_ClearRow(24);
-    CRT_ClearRow(25);
-    CRT_ClearRow(26);
-    CRT_WriteXY(0,24, strings[7]);
-    CRT_WriteXY(0,25, strings[8]);
-    CRT_WriteXY(0,26, strings[9]);
-    gfx_fadein;
-  end;
+  //   CRT_ClearRow(24);
+  //   CRT_ClearRow(25);
+  //   CRT_ClearRow(26);
+  //   CRT_WriteXY(0,24, strings[7]);
+  //   CRT_WriteXY(0,25, strings[8]);
+  //   CRT_WriteXY(0,26, strings[9]);
+  //   gfx_fadein;
+  // end;
 
-  wait(450);
+  // wait(450);
 
-  if skip = false then begin
-    gfx_fadeout;
+  // if skip = false then begin
+  //   gfx_fadeout;
 
-    Waitframe;
-    firstDLI:=@dli_title;
-    DLISTL:= DISPLAY_LIST_ADDRESS_TITLE;
+  //   Waitframe;
+  //   firstDLI:=@dli_title;
+  //   DLISTL:= DISPLAY_LIST_ADDRESS_TITLE;
 
-    // clear screen memory
-    fillbyte(pointer(TXT_ADDRESS), 1320, 0);
-    SetCharset (Hi(CHARSET_ADDRESS)); // when system is off
+  //   // clear screen memory
+  //   fillbyte(pointer(TXT_ADDRESS), 1320, 0);
+  //   SetCharset (Hi(CHARSET_ADDRESS)); // when system is off
 
-    CRT_GotoXY(0,24);
-    CRT_Write(strings[10]);
-    gfx_fadein;
-  end;
+  //   CRT_GotoXY(0,24);
+  //   CRT_Write(strings[10]);
+  //   gfx_fadein;
+  // end;
 
-  wait(450);
+  // wait(450);
 
   if skip = false then begin
     gfx_fadeout;
@@ -272,35 +278,37 @@ begin
     firstDLI:=@dli_pic4_f1;
 
 
-    move(pointer(SCREEN_ADDRESS_PIC4), pointer(TXT_ADDRESS), 1200);
+    // move(pointer(SCREEN_ADDRESS_PIC4), pointer(TXT_ADDRESS), 1200);
+    CRT_ClearRows(0,CRT_screenHeight);
+    move(pointer(SCREEN_ADDRESS_PIC4), pointer(TXT_ADDRESS), 600);
 
-    for y:=0 to 9 do
+    for y:=0 to 8 do
       begin
-        CRT_ClearRow(y + 15);
+        // CRT_ClearRow(y + 15);
         CRT_WriteCentered(y + 15, creditstxt[y]);
       end;
-    CRT_ClearRow(25);  
-    CRT_WriteCentered(25,strings[11]);
+    // CRT_ClearRow(25);  
+    // CRT_WriteCentered(24,strings[11]);
     gfx_fadein;
   end;
 
   count:= 0;
   repeat 
-    inc(count);
+    // inc(count);
     waitframe;
-    if count = 50 then
-    begin
-{$IFDEF PL}
-    CRT_Invert(3,25,34);
-{$ENDIF}
-{$IFDEF DE}
-    CRT_Invert(1,25,38);
-{$ENDIF}
-{$IFDEF EN}
-    CRT_Invert(5,25,29);
-{$ENDIF}
-    count:= 0;
-    end;
+//     if count = 50 then
+//     begin
+// {$IFDEF PL}
+//     CRT_Invert(3,25,34);
+// {$ENDIF}
+// {$IFDEF DE}
+//     CRT_Invert(1,25,38);
+// {$ENDIF}
+// {$IFDEF EN}
+//     CRT_Invert(5,25,29);
+// {$ENDIF}
+//     count:= 0;
+//     end;
     if CRT_Keypressed then skip:= true;
   until skip; //or (count > 450);
 
