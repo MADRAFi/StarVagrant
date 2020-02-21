72bab55f864a684e07636534276a5bd59d610e8b
MADRAFi <madrafi@gmail.com>
2020-02-21 13:46:36 +0100

    fixes to empty cargo update and adding abbuc ver

---
+++ StarV/compilesv_ABBUC.bat
@@ -0,0 +1,6 @@
+@Echo off
+echo Compiling ABBUC
+c:\Users\MADRAFi\Dropbox\Atari\DEV\MADS\mp.exe starv.pas -code:0c00 -define:EN -define:abbuc
+c:\Users\MADRAFi\Dropbox\Atari\DEV\MADS\mads.exe starv.a65 -x -i:c:\Users\MADRAFi\Dropbox\Atari\DEV\MADS\base -o:ABBUC\svdemo_uncmp.xex
+echo.
+pause
