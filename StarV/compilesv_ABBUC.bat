d007e72ba11860ab277112655c604793783e6f47
MADRAFi <madrafi@gmail.com>
2020-03-02 09:04:37 +0100

    fix

--- StarV/compilesv_ABBUC.bat
+++ StarV/compilesv_ABBUC.bat
@@ -3,4 +3,4 @@ echo Compiling ABBUC
 c:\Users\MADRAFi\Dropbox\Atari\DEV\MADS\mp.exe starv.pas -code:0c00 -define:EN -define:abbuc
 c:\Users\MADRAFi\Dropbox\Atari\DEV\MADS\mads.exe starv.a65 -x -i:c:\Users\MADRAFi\Dropbox\Atari\DEV\MADS\base -o:ABBUC\svdemo_uncmp.xex
 echo.
-pause
+rem pause
